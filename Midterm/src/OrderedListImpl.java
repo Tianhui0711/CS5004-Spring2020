10f678969632d11e420ec1af4a250084cb99a629
Tianhui Li <li.tianhu@husky.neu.edu>
2020-02-27 19:53:31 -0800

    Add Midterm

---
+++ Midterm/src/OrderedListImpl.java
@@ -0,0 +1,187 @@
+import java.util.Objects;
+
+public class OrderedListImpl implements OrderedList {
+    private Node head;
+    private int lengthLimit;
+
+    public OrderedListImpl(Node head, int lengthLimit) {
+        this.head = head;
+        this.lengthLimit = lengthLimit;
+    }
+
+    public int getMin() throws IllegalStateException {
+        if (size() == 0) {
+            throw new IllegalStateException("The ordered list is empty.");
+        }
+        return this.head.getValue();
+    }
+
+    public int getMax() throws IllegalStateException {
+        if (size() == 0) {
+            throw new IllegalStateException("The ordered list is empty.");
+        }
+        Node p = head;
+        while (p.getNext() != null) {
+            p = p.getNext();
+        }
+        return p.getValue();
+    }
+
+    public double getMedian() throws IllegalStateException {
+        if (size() == 0) {
+            throw new IllegalStateException("The ordered list is empty.");
+        }
+        int med = 0;
+        Node p = head;
+        while (med < (size() - 1)  / 2) {
+            med += 1;
+            p = p.getNext();
+        }
+        if (size() % 2 == 0) {
+            return (double) (p.getValue() + p.getNext().getValue()) / 2;
+        }
+        return p.getValue();
+    }
+
+    public void add(int val) {
+        if (head == null && size() < lengthLimit) {
+            head = new Node(val);
+            return;
+        }
+
+        if (size() == lengthLimit) {
+            if (val > head.getValue()) {
+                Node p = head;
+                while (p.getValue() < val && p.getNext() != null) {
+                    p = p.getNext();
+                }
+                Node newNext = new Node(val,p.getNext());
+                p.addNext(newNext);
+                Node temp = head;
+                head = temp.getNext();
+                return;
+            }
+        } else {
+            Node p = head;
+            while (p.getValue() < val && p.getNext() != null) {
+                p = p.getNext();
+            }
+            Node newNext = new Node(val,p.getNext());
+            p.addNext(newNext);
+            return;
+        }
+    }
+
+    public OrderedList merge(OrderedList other) {
+        Node p1 = this.head;
+        OrderedListImpl that = (OrderedListImpl) other;
+        Node p2 = that.head;
+        int length = this.lengthLimit + that.lengthLimit;
+        OrderedList mergeList = new OrderedListImpl(null, length);
+        while (p1 != null) {
+            mergeList.add(p1.getValue());
+            p1 = p1.getNext();
+        }
+        while (p2 != null) {
+            mergeList.add(p2.getValue());
+            p2 = p2.getNext();
+        }
+        return mergeList;
+    }
+
+    public void remove(int val) {
+        if (val == head.getValue()) {
+            head = (Node) head.getNext();
+            return;
+        }
+
+        Node p = head;
+        while (p.getNext() != null) {
+            if (val == p.getNext().getValue()) {
+                p.addNext(p.getNext().getNext());
+                return;
+            }
+            p = p.getNext();
+        }
+    }
+
+    public void removeAll(int cap) {
+        if (head.getValue() >= cap) {
+            head = null;
+            return;
+        }
+
+        Node p = head;
+        while (p.getNext().getValue() < cap) {
+            p = p.getNext();
+        }
+        p.addNext(null);
+    }
+
+    public boolean isEmpty() {
+        if (size() == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public OrderedList intersection(OrderedList other) {
+        return null;
+    }
+
+    public OrderedList union(OrderedList other) {
+        return null;
+    }
+
+    public int size() {
+        int size = 0;
+        Node p = head;
+        while (p != null) {
+            size += 1;
+            p = p.getNext();
+        }
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        if (head == null) {
+            return "[]";
+        }
+
+        String s = "[";
+        Node p = head;
+        while (p != null) {
+            s += String.valueOf(p.getValue()) + " ";
+            p = p.getNext();
+        }
+        if (size() < lengthLimit) {
+            for (int i = 0; i < lengthLimit - size(); i++) {
+                s += "?";
+            }
+        }
+        s = s.trim();
+        s += "]";
+        return s;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderedListImpl that = (OrderedListImpl) o;
+        int sum1 = 0;
+        int sum2 = 0;
+        Node p1 = this.head;
+        Node p2 = that.head;
+        while (p1 != null) {
+            sum1 += p1.getValue();
+            p1 = p1.getNext();
+        }
+        while (p2 != null) {
+            sum2 += p2.getValue();
+            p2 = p2.getNext();
+        }
+        return lengthLimit == that.lengthLimit && sum1 == sum2;
+    }
+}
---
+++ Midterm/test/OrderedListImplTest.java
@@ -0,0 +1,98 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class OrderedListImplTest {
+    OrderedListImpl list;
+    Node node1;
+    Node node2;
+
+    @Before
+    public void setUp() throws Exception {
+        node1 = new Node(1);
+        node1.addNext(new Node(2));
+        node1.getNext().addNext(new Node(3));
+        list = new OrderedListImpl(node1, 4);
+    }
+
+    @Test
+    public void getMin() {
+        assertEquals(1, list.getMin());
+    }
+
+    @Test
+    public void getMax() {
+        assertEquals(3, list.getMax());
+    }
+
+    @Test
+    public void getMedian() {
+        list.getMedian();
+        assertEquals(2, list.getMedian(), 0);
+
+        list.add(4);
+        assertEquals(2.5, list.getMedian(), 0);
+    }
+
+    @Test
+    public void add() {
+        list.add(4);
+        assertEquals("[1 2 3 4]", list.toString());
+
+        list.add(1);
+        assertEquals("[1 2 3 4]", list.toString());
+
+        list.add(5);
+        assertEquals("[2 3 4 5]", list.toString());
+
+        list.add(5);
+        assertEquals("[3 4 5 5]", list.toString());
+    }
+
+    @Test
+    public void merge() {
+        node2 = new Node(4);
+        node2.addNext(new Node(5));
+        node2.getNext().addNext(new Node(6));
+        OrderedListImpl list2 = new OrderedListImpl(node2, 4);
+
+    }
+
+    @Test
+    public void remove() {
+        list.remove(2);
+        assertEquals("[1 3 ??]", list.toString());
+    }
+
+    @Test
+    public void removeAll() {
+        list.removeAll(2);
+        assertEquals("[1 ???]", list.toString());
+
+        list.removeAll(1);
+        assertTrue(list.isEmpty());
+    }
+
+    @Test
+    public void isEmpty() {
+        assertFalse(list.isEmpty());
+        assertTrue(new OrderedListImpl(null, 0).isEmpty());
+    }
+
+    @Test
+    public void size() {
+        assertEquals(3, list.size());
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("[1 2 3 ?]", list.toString());
+    }
+
+    @Test
+    public void testEquals() {
+    }
+}
